cd5199773ea25f0a4854eb883d2a407ca7572f7a
Your Name <you@example.com>
2020-10-20 12:41:40 +0200

    iniciar la aplicación

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+class Server {
+    private app: express.Application
+    constructor(){
+        this.app = express()
+        this.config()
+    }
+    config(){
+        this.app.set('port', process.env.PORT || 3000)
+    }
+    routes(){
+
+    }
+    start(){
+        this.app.listen(this.app.get('port'), 
+        () => {
+            console.log(`Server on port: ${this.app.get('port')}`)
+        })
+    }
+}
+const server = new Server()
+server.start()
